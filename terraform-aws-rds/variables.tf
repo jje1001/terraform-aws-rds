f9a229694c21863d2239e11cc04769ef4a9ea8cf
jje1001 <jje1001@insideinfo.co.kr>
2025-01-13 15:18:22 +0900

    git initial

---
+++ terraform-aws-rds/variables.tf
@@ -0,0 +1,27 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "db_name" {
+  description = "Unique name to assign to RDS instance"
+}
+
+variable "db_username" {
+  description = "RDS root username"
+}
+
+variable "db_password" {
+  description = "RDS root user password"
+  sensitive   = true
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
